cb912b6c78242892990b044682cc194baf2af87a
hosonhao2k17 <sonha@sonhaodz>
2026-01-22 10:16:47 +0700

    feat(role): Create role dto

--- src/api/roles/dto/create-role.dto.ts
+++ src/api/roles/dto/create-role.dto.ts
@@ -1,6 +1,6 @@
 import { IsEnum } from "class-validator";
 import { RoleStatus } from "src/common/constants/status.constant";
-import { StringField } from "src/decorators/field.decorator";
+import { EnumField, StringField } from "src/decorators/field.decorator";
 
 export class CreateRoleDto {
 
@@ -10,6 +10,9 @@ export class CreateRoleDto {
     @StringField()
     title: string;
 
-    @IsEnum(RoleStatus)
-    status: RoleStatus;
+    @EnumField(RoleStatus, {options: true})
+    status?: RoleStatus;
+
+    @StringField({options: true})
+    description?: string;
 }
