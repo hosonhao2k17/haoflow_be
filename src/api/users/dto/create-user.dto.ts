d9afa22cb08f7df3318399abed17f2d3ab10acd8
hosonhao2k17 <sonha@sonhaodz>
2026-01-27 20:00:01 +0700

    feat(user): create user dto

--- src/api/users/dto/create-user.dto.ts
+++ src/api/users/dto/create-user.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserDto {}
+import { IsUUID } from "class-validator";
+import { Gender, UserStatus } from "src/common/constants/app.constant";
+import { DateField, EmailField, EnumField, PasswordField, StringField, UrlField } from "src/decorators/field.decorator";
+
+export class CreateUserDto {
+
+    @StringField()
+    fullName: string;
+
+    @EmailField()
+    email: string;
+
+    @UrlField({options: true})
+    avatar?: string;
+
+    @PasswordField()
+    password: string;
+
+    @EnumField(Gender,{options: true})
+    gender?: Gender;
+
+    @EnumField(UserStatus,{options: true})
+    status?: UserStatus;
+
+    @IsUUID()
+    roleId: string;
+
+    @DateField({options: true})
+    birthDate?: Date;
+}
