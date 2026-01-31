c6cdc8c0843a02d596f577634fcae583b1133a74
hosonhao2k17 <sonha@sonhaodz>
2026-01-31 15:35:56 +0700

    chore: mail module

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+@Module({
+  imports: [MailerModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      transport: {
+        host: configService.get('MAIL_HOST'),
+        port: configService.get('MAIL_PORT'),
+        secure: false,
+        auth: {
+          user: configService.get('MAIL_USER'),
+          pass: configService.get('MAIL_PASS')
+        },
+        defaults: {
+          from: ''
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }
+    })
+  })],
+  providers: [MailService],
+  exports: [MailService]
+})
+export class MailModule {}
