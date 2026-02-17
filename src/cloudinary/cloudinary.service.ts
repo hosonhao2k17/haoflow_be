5888e1eb5a97b38ebce642929cd57847996b6fe1
hosonhao2k17 <sonha@sonhaodz>
2026-02-17 07:52:30 +0700

    chore: upload single and upload multi cloudinary

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { v2 as Cloudinary } from 'cloudinary';
+import { CLOUDINARY } from 'src/common/constants/app.constant';
+import { UploadApiResponse } from 'cloudinary';
+import { Readable } from 'stream';
 
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryService {
+  constructor(
+    @Inject(CLOUDINARY)
+    private readonly cloudinary: typeof Cloudinary,
+  ) {}
+
+    async uploadSingle(
+        file: Express.Multer.File,
+        folder: string = 'uploads',
+    ): Promise<UploadApiResponse> {
+        return new Promise((resolve, reject) => {
+            const upload = this.cloudinary.uploader.upload_stream(
+                {
+                    folder,
+                    resource_type: 'auto',
+                },
+                (error, result) => {
+                    if (error) return reject(error);
+                    resolve(result!);
+                },
+            );
+
+            const stream = Readable.from(file.buffer);
+            stream.pipe(upload);
+        });
+    }
+
+    async deleteFile(publicId: string) {
+        return this.cloudinary.uploader.destroy(publicId);
+    }
+    
+    async uploadMulti(
+        files: Express.Multer.File[],
+        folder: string = 'uploads'
+    ) {
+        const results = await Promise.all(
+            files.map((file) => this.uploadSingle(file, folder))
+        )
+        return results.map((result) => ({
+            url: result.secure_url,
+            publicId: result.public_id,
+        }));
+    }
+}
